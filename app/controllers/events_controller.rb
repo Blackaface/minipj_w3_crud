18b5a2328aa142f9a224ace7a160a8ee9258ad97
Fang Ernesto <zxc9799979@gmail.com>
2016-11-06 22:29:31 +0800

    page show and create

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,4 +5,20 @@ class EventsController < ApplicationController
 	def new
 		@event = Event.new
 	end
+
+	def create
+		@event = Event.new(event_params)
+		@event.save
+
+		redirect_to events_path
+	end
+	def show
+		@event = Event.find(params[:id])
+	end
+
+
+private
+	def event_params
+		params.require(:event).permit(:name, :description, :isbn)
+	end
 end
