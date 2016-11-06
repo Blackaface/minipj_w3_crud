6f91e3312b05a4d407a2a4e535acf10b60ecdc00
Fang Ernesto <zxc9799979@gmail.com>
2016-11-07 01:08:10 +0800

    destroy function

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,9 +8,11 @@ class EventsController < ApplicationController
 
 	def create
 		@event = Event.new(event_params)
-		@event.save
-
-		redirect_to events_url
+		if @event.save
+		   redirect_to events_url
+		else
+			render :action => :new
+		end
 	end
 	def show
 		@event = Event.find(params[:id])
@@ -21,10 +23,19 @@ class EventsController < ApplicationController
 	end
 	def update
 		@event = Event.find(params[:id])
-		@event.update(event_params)
-
-		redirect_to event_url(@event)
+		if @event.update(event_params)
+		   redirect_to event_url(@event)
+		else
+			render :action => :edit
+		end
 	end
+	def destroy
+		@event = Event.find(params[:id])
+		@event.destroy
+
+		redirect_to events_url
+	end
+
 
 
 private
