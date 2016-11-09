b8ffd8aaed523150aacb202488795d9dbb47d7dd
Fang Ernesto <zxc9799979@gmail.com>
2016-11-09 14:33:47 +0800

    db migrate change

--- db/migrate/20161106034608_rename_description.rb
+++
@@ -1,6 +0,0 @@
-class RenameDescription < ActiveRecord::Migration[5.0]
-  def change
-  	add_column :events, :description, :text
-  	remove_column :events, :description, :text
-  end
-end
--- db/migrate/20161106035339_rename_description_again.rb
+++
@@ -1,6 +0,0 @@
-class RenameDescriptionAgain < ActiveRecord::Migration[5.0]
-  def change
-  	add_column :events, :description, :text
-  	remove_column :events, :descrioption, :text
-  end
-end
